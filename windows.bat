aa495daf3c9e8eb2aeac683589ad6806a7b3c576
Chris Barrett <chris@codesaru.com>
2020-07-14 08:42:40 -0700

    Configure Alacritty on Windows.

--- windows.bat
+++ windows.bat
@@ -1,10 +1,23 @@
 @ECHO OFF
+setlocal
 echo "REM: Run this as admin."
 
 REM Alacritty
-mklink /J "%APPDATA%\alacritty" alacritty
+choice /M "Config alacritty?"
+if /I "%ERRORLEVEL%" neq "1" goto no_alacritty
+echo "Configuring alacritty..."
+del "%APPDATA%\alacritty"
+mkdir "%APPDATA%\alacritty"
+mklink /H "%APPDATA%\alacritty\alacritty.yml" alacritty\alacritty-windows.yml
+:no_alacritty
 
 REM Vim
-mklink /H "%USERPROFILE%\.vimrc" vim\vimrc
-mklink /J "%USERPROFILE%\.vim" vim
+choice /M "Config vim?"
+if /I "%ERRORLEVEL%" neq "1" goto no_vim
+echo "Configuring vim..."
+REM mklink /H "%USERPROFILE%\.vimrc" vim\vimrc
+REM mklink /J "%USERPROFILE%\.vim" vim
+:no_vim
+
+endlocal
 
