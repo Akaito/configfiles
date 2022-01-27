dd912a7ba423c45abd3081ace58b9fa7d8bd0f38
Chris Barrett <chris@codesaru.com>
2022-01-27 14:06:39 -0800

    Commit messages still saving cursor pos...
    - Fix echo quoting style in windows.bat.

--- windows.bat
+++ windows.bat
@@ -5,7 +5,7 @@ REM echo "REM: Run this as admin... Maybe?"
 REM Alacritty
 choice /M "Config alacritty?"
 if /I "%ERRORLEVEL%" neq "1" goto no_alacritty
-echo "Configuring alacritty..."
+echo Configuring alacritty...
 del "%APPDATA%\alacritty"
 mkdir "%APPDATA%\alacritty"
 mklink /H "%APPDATA%\alacritty\alacritty.yml" alacritty\alacritty.windows.yml
@@ -14,7 +14,7 @@ mklink /H "%APPDATA%\alacritty\alacritty.yml" alacritty\alacritty.windows.yml
 REM Git
 choice /M "Config git?"
 if /I "%ERRORLEVEL%" neq "1" goto no_git
-echo "Configuring git..."
+echo Configuring git...
 del "%USERPROFILE%\.gitconfig"
 mklink /H "%USERPROFILE%\.gitconfig" git\gitconfig
 :no_git
@@ -22,11 +22,12 @@ mklink /H "%USERPROFILE%\.gitconfig" git\gitconfig
 REM Vim
 choice /M "Config vim?"
 if /I "%ERRORLEVEL%" neq "1" goto no_vim
-echo "Configuring vim..."
+echo Configuring vim...
 rmdir "%USERPROFILE%\vimfiles"
 del "%USERPROFILE%\.vimrc"
 mklink /J "%USERPROFILE%\vimfiles" vim
 mklink /H "%USERPROFILE%\.vimrc" vim\vimrc
+echo   Doing Plug updates...
 vim -c PlugUpgrade -c PlugUpdate -c qa
 :no_vim
 
