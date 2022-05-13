a587058d78cd5781cb09768055383a9855336f30
Chris Barrett <chris@codesaru.com>
2022-05-13 10:45:43 -0400

    Add pop_os power script.

---
+++ power-charge-thresholds.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+sudo system76-power charge-thresholds --list-profiles
+sudo system76-power charge-thresholds
+echo 'REM: sudo system76-power charge-thresholds --profile max_lifespan'
+
